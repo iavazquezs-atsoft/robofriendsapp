3443c4aa2452328c56b7e376eef094348031d7c7
Ivan Alonso Vazquez Serrano <iavazquezs@liverpool.com.mx>
2020-01-23 16:26:14 -0600

    Robofriends with sega font familly

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+//import Hello from './Hello'
+//import Card from './Card'
+//import CardList from './CardList'
+import Main from './Main'
 import * as serviceWorker from './serviceWorker';
+import 'tachyons'
+//import { robots } from './robots';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(<Main />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
