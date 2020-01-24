56c63745a38a3f3fde7f46998e6669d2d77e2261
Ivan Alonso Vazquez Serrano <iavazquezs@liverpool.com.mx>
2020-01-24 10:54:36 -0600

    ErrorBoundry
    ErrorBoundry

--- robofriends/src/containers/Main.js
+++ robofriends/src/containers/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
 //import { robots } from './robots'
 import './Main.css'
 
@@ -57,7 +58,9 @@ class Main extends Component {
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}  />
                     <Scroll>
-                        <CardList robots={ filteredRobots } />
+                        <ErrorBoundry>
+                            <CardList robots={ filteredRobots } />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
                 
